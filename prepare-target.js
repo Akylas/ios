af78b5fe42ae4317fdd982891463444ed54cfe80
Martin Guillon <dev@akylas.fr>
2024-04-09 15:31:31 +0200

    chore: package name change

--- prepare-target.js
+++ prepare-target.js
@@ -7,7 +7,7 @@ const target = cmdArgs[0]; // ios or visionos
 const packagePath = path.join('package.json');
 const packageJson = JSON.parse(fs.readFileSync(packagePath));
 
-packageJson.name = `@nativescript/${target}`;
+packageJson.name = `@akylas/nativescript-${target}-runtime`;
 packageJson.description = `NativeScript Runtime for ${target === 'ios' ? 'iOS' : 'visionOS'}`;
 
 fs.writeFileSync(packagePath, JSON.stringify(packageJson, null, 2));
